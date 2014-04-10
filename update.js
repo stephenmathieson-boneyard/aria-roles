2f2e26f679e68110e77da3cb74feeb553a4bdc5d
Stephen Mathieson <smath23@gmail.com>
2014-04-10 15:40:46 -0400

    fix #1

--- update.js
+++ update.js
@@ -4,24 +4,43 @@ var jquery = require('cheerio');
 var fs = require('fs');
 
 var roles = [];
+var types = {};
 
 request
 .get('http://www.w3.org/TR/wai-aria/roles')
 .end(function (err, res) {
   if (err) throw err;
   var $ = jquery.load(res.text);
-  var $index = $('#index_role');
-  var $roles = $index.find('dt a');
-  $roles.each(function () {
-    var $this = jquery(this);
-    var role = $this.text().replace('(abstract role)', '').trim()
-    roles.push(role);
+
+
+  var $groups = $('#roles_categorization .section[role=region]');
+
+  $groups.each(function () {
+    var $group = $(this);
+    var title = $group.attr('id').split('_')[0];
+    var $roles = $group.find('ul li a code');
+    types[title] = [];
+    $roles.each(function () {
+      var $role = $(this);
+      var role = $role.text();
+
+      roles.push(role);
+      types[title].push(role);
+    });
   });
 
-  var str = 'module.exports = ' + JSON.stringify(roles, null, 2);
-  fs.writeFile('./index.js', str, function (err) {
-    if (err) throw err;
-    console.log('found %d roles', roles.length);
-  });
+  var js = [
+    'var roles = ' + JSON.stringify(roles, null, 2)
+  ];
+
+  for (var type in types) {
+    js.push('roles["' + type + '"] = ' + JSON.stringify(types[type], null, 2));
+  }
+
+  js.push('module.exports = roles;');
+
+  fs.writeFileSync('./index.js', js.join('\n'));
+  console.log('%d roles', roles.length);
+
 });
 
