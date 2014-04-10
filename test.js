4410c366c63a958f3719562cde32ab81f16d3376
Stephen Mathieson <smath23@gmail.com>
2014-04-10 15:46:33 -0400

    test: Ensure all enumerable elements are strings

--- test.js
+++ test.js
@@ -8,3 +8,7 @@ assert(roles.abstract && roles.abstract.length);
 assert(roles.document && roles.document.length);
 assert(roles.landmark && roles.landmark.length);
 assert(roles.widget && roles.widget.length);
+
+for (var i = 0; i < roles.length; i++) {
+  assert('string' == typeof roles[i])
+}
